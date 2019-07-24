911ad386af15c7cbd7f01c7cc34eb7a212406be1
mrjctech <mrjctech@gmail.com>
2019-07-25 00:49:55 +0800

    navigation

---
+++ navigation/src/main/java/com/chenqiao/navigation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.chenqiao.navigation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp() =
+            findNavController(this, R.id.my_nav_host_fragment).navigateUp()
+}
---
+++ navigation/src/main/java/com/chenqiao/navigation/fragment/Page2Fragment.kt
@@ -0,0 +1,16 @@
+package com.chenqiao.navigation.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chenqiao.navigation.R
+
+class Page2Fragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_page3, container, false)
+    }
+}
