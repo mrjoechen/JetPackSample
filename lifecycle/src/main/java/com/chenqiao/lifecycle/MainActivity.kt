c0115b7922b9d4d05a580f763b790c6b5599b4ec
mrjctech <mrjctech@gmail.com>
2019-07-28 23:08:43 +0800

    Lifecycle

--- lifecycle/src/main/java/com/chenqiao/lifecycle/MainActivity.kt
+++ lifecycle/src/main/java/com/chenqiao/lifecycle/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.chenqiao.lifecycle
 
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.LifecycleRegistry
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , LifecycleOwner {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getLifecycle().addObserver(MyObserver())
+
     }
+
 }
---
+++ lifecycle/src/main/java/com/chenqiao/lifecycle/MyObserver.kt
@@ -0,0 +1,19 @@
+package com.chenqiao.lifecycle
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+
+
+class MyObserver : LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun connectListener() {
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun disconnectListener() {
+
+    }
+}
