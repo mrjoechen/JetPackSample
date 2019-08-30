78a6b283f48d24e97b8a6e7b54fdfce83d551d3b
mrjctech <mrjctech@gmail.com>
2019-08-30 23:59:13 +0800

    Lifecycle

--- lifecycle/src/main/java/com/chenqiao/lifecycle/MainActivity.kt
+++ lifecycle/src/main/java/com/chenqiao/lifecycle/MainActivity.kt
@@ -11,9 +11,7 @@ class MainActivity : AppCompatActivity() , LifecycleOwner {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         getLifecycle().addObserver(MyObserver())
-
     }
 
 }
