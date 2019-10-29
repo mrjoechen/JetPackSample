a8cbdd4038c03d1424066782cb4944d0370b93db
mrjctech <mrjctech@gmail.com>
2019-10-29 23:10:18 +0800

    room

--- room/src/main/java/com/chenqiao/room/MainActivity.kt
+++ room/src/main/java/com/chenqiao/room/MainActivity.kt
@@ -2,11 +2,21 @@ package com.chenqiao.room
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.room.Room
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val db = Room.databaseBuilder(applicationContext,
+                AppDatabase::class.java, "database-name").build()
     }
 }
---
+++ room/src/main/java/com/chenqiao/room/User.java
@@ -0,0 +1,46 @@
+package com.chenqiao.room;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int uid;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    // Getters and setters are ignored for brevity,
+    // but they're required for Room to work.
+
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
